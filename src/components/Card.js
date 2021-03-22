2c20293745bd9b65633082dbd48ad30a48bf7740
SBridgens <simon.bridgens@gmail.com>
2021-03-22 21:28:38 +0000

    change to cats ;)

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,10 @@ import React from "react";
 const Card = ({ name, email, id }) => {
   return (
     <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img alt="robots" src={`https://robohash.org/${id}?size=200x200`}></img>
+      <img
+        alt="robots"
+        src={`https://robohash.org/${id}?size=200x200&set=set4`}
+      ></img>
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
